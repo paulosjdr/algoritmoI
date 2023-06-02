aa22a386ecabdfceade7062512c1268a3555354c
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-02 20:23:32 -0300

    contadorLinhas.c

--- contadorLinhas.c
+++ contadorLinhas.c
@@ -4,8 +4,27 @@
 
 int main()
 {
-    for(int i = 0; i<5 ; i++){ //contador
-        printf("*\n");
+    /*
+    i = 0     (Inicialização da variável)
+    i < linhas(Contador das linhas)
+    i++       (Variável + 1)
+    */
+
+    int linhas;
+    printf("INFORME A QUANTIDADE DE LINHAS: ");
+    scanf("%d", &linhas);
+    for(int i = 0; i<linhas ; i++){ //contador
+        for(int j = 0; j < i; j++){ //contador
+            if(j%2!=0)
+                printf("*"); //repetindo o nr da linha
+            else
+                printf("%d",j);
+        }
+        if(i%2!=0)
+            printf("*\n");
+        else
+            printf("%d\n",i);
+
     }
 
     return 0;
