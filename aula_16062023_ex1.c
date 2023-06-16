02755b81cfff9de585d7fa1c6e69d390d6efbe85
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-16 19:36:54 -0300

    Create aula_16062023_ex1.c

---
+++ aula_16062023_ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float notas[5];
+    float soma;
+
+    //fazer a leitura das notas
+    for(int i=0; i < 5; i++){
+        printf("DIGITE A NOTA %d DO ALUNO: ", i+1);
+        scanf("%f", &notas[i]);
+
+
+    }
+    //fazer a soma das notas
+    for(int i=0; i < 5; i++){
+        soma = soma + notas[i];
+
+    }
+
+    printf("NOTA TOTAL = %.2f", soma);
+
+    return 0;
+}
