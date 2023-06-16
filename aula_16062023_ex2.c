5c66441b5d5007488a102be2bbd16532d62d4d55
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-16 19:46:24 -0300

    Create aula_16062023_ex2.c

---
+++ aula_16062023_ex2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define QTDE_NOTAS 5
+
+int main()
+{
+    float notas[QTDE_NOTAS];
+    float soma = 0;
+
+    //fazer a leitura das notas
+    for(int i = 0; i < QTDE_NOTAS; i++){
+        printf("DIGITE A NOTA %d DO ALUNO: ", i+1);
+        scanf("%f", &notas[i]);
+
+    }
+
+    //fazer a soma das notas
+    for(int i = 0; i < QTDE_NOTAS; i++){
+        soma = soma + notas[i];
+
+    }
+
+    printf("NOTA TOTAL = %.2f", soma);
+
+    return 0;
+}
