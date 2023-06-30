d2007d840a6391e01fcc39d18ee511edfb8eb902
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-06-29 22:29:27 -0300

    Create aula_29062023_funções_ex3.c

---
+++ aula_29062023_funções_ex3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//int R;
+
+int SOMA(int X, INT Y)
+{
+
+    return X+Y;
+
+}
+
+int main()
+{
+   //printf("INICIO\n");
+
+   scanf("%d", &A);
+   scanf("%d", &B);
+
+   R = SOMA(A,B);
+
+   //R = A+B;
+
+   printf("a+b = %d", R);
+
+}
